e26aa61986c17e3b144a49d2e30dfaff74be208e
Luis H. Hernandez <guicholh@gmail.com>
2024-07-31 11:02:53 -0500

    nightwing

--- post.sh
+++ post.sh
@@ -6,7 +6,7 @@
 #   -F 'image=@cousins_id.jpg'
 
 curl -v -X POST \
-  http://b0wc0g4.5.161.177.10.sslip.io/extract-text \
+    http://k8cs404.5.161.177.10.sslip.io/extract-text \
   -H 'Content-Type: multipart/form-data' \
   -F 'image=@cousins_id.jpg'
 # curl -v localhost:5000/ping
