491c06291d56d6934ea450d7b0069115f601fe6e
Luis H. Hernandez <guicholh@gmail.com>
2024-07-31 10:38:59 -0500

    aquaman

--- post.sh
+++ post.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
+# curl -v -X POST \
+#   127.0.0.1:5000/extract-text \
+#   -H 'Content-Type: multipart/form-data' \
+#   -F 'image=@cousins_id.jpg'
+
 curl -v -X POST \
-  127.0.0.1:5000/extract-text \
+  http://b0wc0g4.5.161.177.10.sslip.io/extract-text \
   -H 'Content-Type: multipart/form-data' \
   -F 'image=@cousins_id.jpg'
-
 # curl -v localhost:5000/ping
